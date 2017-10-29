081429da3b5dc2504e859b7a77777c5759fab370
Andreas Pardeike <andreas@pardeike.net>
2017-10-29 12:09:40 +0100

    Add vanilla method logging (off, just for debugging)

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,7 +1,13 @@
 ﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 using Verse;
+using Verse.AI;
 
 namespace ZombieLand
 {
@@ -23,6 +29,16 @@ namespace ZombieLand
 
 			// extra patch for Combat Extended
 			Patches.Projectile_Launch_Patch.PatchCombatExtended(harmony);
+
+			// for debugging
+			/*
+			DebugRimworldMethodCalls((Type type) =>
+			{
+				if (type.Name.Contains("AttackTarget")) return true;
+				if (type.Name.Contains("_AI")) return true;
+				if (type.Name.Contains("Reachability")) return true;
+				return false;
+			}); */
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect)
@@ -50,6 +66,49 @@ namespace ZombieLand
 			if (world != null && world.components != null)
 				return "ZombielandGameSettings".Translate();
 			return "ZombielandDefaultSettings".Translate();
+		}
+
+		// for debugging
+
+		static IEnumerable<CodeInstruction> MethodCallsTranspiler(IEnumerable<CodeInstruction> instructions, MethodBase method)
+		{
+			yield return new CodeInstruction(OpCodes.Ldstr, method.DeclaringType.FullName + "." + method.Name);
+			yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Log), "Warning"));
+			foreach (var instruction in instructions)
+				yield return instruction;
+		}
+		public void DebugRimworldMethodCalls(Func<Type, bool> typeFilter)
+		{
+			var harmony = HarmonyInstance.Create("net.pardeike.zombieland.debug");
+			var transpiler = new HarmonyMethod(AccessTools.Method(typeof(ZombielandMod), "MethodCallsTranspiler"));
+			var patches = new HashSet<MethodBase>();
+
+			var asm = Assembly.GetAssembly(typeof(Job));
+			var types = asm.GetTypes();
+			Array.Sort(types, (a, b) => { return a.FullName.CompareTo(b.FullName); });
+			for (var i = 0; i < types.Length; i++)
+			{
+				var type = types[i];
+				if (typeFilter(type) == false) continue;
+				var methods = type.GetMethods(AccessTools.all);
+				Array.Sort(methods, (a, b) => { return a.Name.CompareTo(b.Name); });
+				for (var j = 0; j < methods.Length; j++)
+				{
+					var method = methods[j];
+					if (method.IsAbstract) continue;
+					if (method.DeclaringType != type) continue;
+					if (patches.Contains(method)) continue;
+					patches.Add(method);
+					try
+					{
+						harmony.Patch(method, null, null, transpiler);
+					}
+					catch (Exception ex)
+					{
+						Log.Error("Exception patching " + method + ": " + ex);
+					}
+				}
+			}
 		}
 	}
 }
