dfc5371021b891af3a3a3aa5e543d4826c4592f3
Andreas Pardeike <andreas@pardeike.net>
2018-10-06 00:20:57 +0200

    Works with Rimworld 0.19 and 1.0

--- Source/MultiVersionModFix.cs
+++ Source/MultiVersionModFix.cs
@@ -1,56 +1,47 @@
 ﻿using Harmony;
 using RimWorld;
+using Steamworks;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Xml;
 using Verse;
+using Verse.Steam;
 
-namespace SaneModdersWorld
+[HarmonyPatch(typeof(ModMetaData))]
+[HarmonyPatch("VersionCompatible", MethodType.Getter)]
+public static class MultiVersionModFix1
 {
-	[HarmonyPatch(typeof(ModMetaData))]
-	[HarmonyPatch("VersionCompatible", MethodType.Getter)]
-	[StaticConstructorOnStartup]
-	public static class MultiVersionModFix
+	public static ModContentPack GetPack()
 	{
-		static readonly FastInvokeHandler translateHandler = null;
-		const string methodName = "Translate";
-		const string oldClassName = "Verse.Translator";
-		const string newClassName = "Verse.TranslatorFormattedStringExtensions";
-
-		static MultiVersionModFix()
-		{
-			bool IsTranslateMethod(MethodInfo method) =>
-				method.Name == methodName &&
-				method.GetParameters().Count() == 2 &&
-				method.GetParameters()[0].ParameterType == typeof(string) &&
-				method.GetParameters()[1].ParameterType.IsArray;
-
-			var oldTranslate = AccessTools.TypeByName(oldClassName).GetMethods()
-				.FirstOrDefault(IsTranslateMethod);
-			if (oldTranslate != null)
-			{
-				translateHandler = MethodInvoker.GetHandler(oldTranslate);
-				return;
-			}
-
-			var newTranslate = AccessTools.TypeByName(newClassName).GetMethods()
-				.FirstOrDefault(IsTranslateMethod);
-			translateHandler = MethodInvoker.GetHandler(newTranslate);
-		}
-
-		static void Postfix(ModMetaData __instance, ref bool __result)
-		{
-			var thisAssembly = typeof(MultiVersionModFix).Assembly;
-			var modContentPack = LoadedModManager.RunningModsListForReading
+		var thisAssembly = typeof(MultiVersionModFix1).Assembly;
+		return LoadedModManager.RunningModsListForReading
 				.FirstOrFallback(mod => mod.assemblies.loadedAssemblies.Contains(thisAssembly));
+	}
 
-			if (VersionControl.CurrentMajor == 0 && VersionControl.CurrentMinor >= 19)
-				if (__instance.Identifier == modContentPack.Identifier)
-					__result = true;
-		}
+	static void Postfix(ModMetaData __instance, ref bool __result)
+	{
+		if (VersionControl.CurrentMajor == 0 && VersionControl.CurrentMinor >= 19)
+			if (__instance.Identifier == GetPack().Identifier)
+				__result = true;
+	}
+}
 
-		public static string Translate(this string key, params object[] args)
-		{
-			return translateHandler(null, new object[] { key, args }) as string;
-		}
+[HarmonyPatch(typeof(Workshop))]
+[HarmonyPatch("SetWorkshopItemDataFrom")]
+public static class MultiVersionModFix2
+{
+	static IList<string> GetTags(string rootDir)
+	{
+		var xml = new XmlDocument();
+		xml.Load(rootDir + Path.DirectorySeparatorChar + "About" + Path.DirectorySeparatorChar + "Manifest.xml");
+		return xml.SelectNodes("/Manifest/tags/li").Cast<XmlNode>().Select(node => node.InnerText).ToList();
+	}
+
+	static void Postfix(UGCUpdateHandle_t updateHandle, WorkshopItemHook hook)
+	{
+		var rootDir = MultiVersionModFix1.GetPack().RootDir;
+		if (rootDir == hook.Directory.FullName)
+			SteamUGC.SetItemTags(updateHandle, GetTags(rootDir));
 	}
 }
