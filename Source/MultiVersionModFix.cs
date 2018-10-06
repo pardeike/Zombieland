e470e2accc48d66d25ae7c19618a686f561c3da0
Andreas Pardeike <andreas@pardeike.net>
2018-10-06 23:25:58 +0200

    Better version tags

--- Source/MultiVersionModFix.cs
+++ Source/MultiVersionModFix.cs
@@ -4,45 +4,78 @@ using Steamworks;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Xml;
 using Verse;
 using Verse.Steam;
 
-[HarmonyPatch(typeof(ModMetaData))]
-[HarmonyPatch("VersionCompatible", MethodType.Getter)]
-public static class MultiVersionModFix1
+[StaticConstructorOnStartup]
+static class MultiVersionModFix
 {
-	public static ModContentPack GetPack()
+	const string _multiversionmodfix = "multiversionmodfix";
+	static readonly Dictionary<string, List<System.Version>> cachedVersions = new Dictionary<string, List<System.Version>>();
+
+	static MultiVersionModFix()
 	{
-		var thisAssembly = typeof(MultiVersionModFix1).Assembly;
-		return LoadedModManager.RunningModsListForReading
-				.FirstOrFallback(mod => mod.assemblies.loadedAssemblies.Contains(thisAssembly));
+		var instance = HarmonyInstance.Create(_multiversionmodfix);
+		var aBool = false;
+		var m_VersionCompatible = AccessTools.Property(typeof(ModMetaData), nameof(ModMetaData.VersionCompatible)).GetGetMethod();
+		var m_VersionCompatiblePostfix = SymbolExtensions.GetMethodInfo(() => VersionCompatible_Postfix(null, ref aBool));
+		instance.PostfixOnce(m_VersionCompatible, m_VersionCompatiblePostfix);
+		var m_SetWorkshopItemDataFrom = AccessTools.Method(typeof(Workshop), "SetWorkshopItemDataFrom");
+		var m_SetWorkshopItemDataFrom_Postfix = SymbolExtensions.GetMethodInfo(() => SetWorkshopItemDataFrom_Postfix(default(UGCUpdateHandle_t), null));
+		instance.PostfixOnce(m_SetWorkshopItemDataFrom, m_SetWorkshopItemDataFrom_Postfix);
 	}
 
-	static void Postfix(ModMetaData __instance, ref bool __result)
+	static void PostfixOnce(this HarmonyInstance instance, MethodInfo original, MethodInfo postfix)
 	{
-		if (VersionControl.CurrentMajor == 0 && VersionControl.CurrentMinor >= 19)
-			if (__instance.Identifier == GetPack().Identifier)
-				__result = true;
-	}
-}
-
-[HarmonyPatch(typeof(Workshop))]
-[HarmonyPatch("SetWorkshopItemDataFrom")]
-public static class MultiVersionModFix3
-{
-	static IList<string> GetTags(string rootDir)
-	{
-		var xml = new XmlDocument();
-		xml.Load(rootDir + Path.DirectorySeparatorChar + "About" + Path.DirectorySeparatorChar + "Manifest.xml");
-		var tags = xml.SelectNodes("/Manifest/targetVersions/li").Cast<XmlNode>().Select(node => (node.InnerText + "$").Replace(".0$", ""));
-		return tags.Add("Mod").ToList();
+		var postfixes = instance.GetPatchInfo(original)?.Postfixes;
+		if (postfixes == null || !postfixes.Any(patch => patch != null && patch.owner == _multiversionmodfix))
+			instance.Patch(original, postfix: new HarmonyMethod(postfix));
 	}
 
-	static void Postfix(UGCUpdateHandle_t updateHandle, WorkshopItemHook hook)
+	static List<System.Version> GetTaggedVersions(string rootDir)
 	{
-		var rootDir = MultiVersionModFix1.GetPack().RootDir;
-		if (rootDir == hook.Directory.FullName)
-			SteamUGC.SetItemTags(updateHandle, GetTags(rootDir));
+		if (cachedVersions.TryGetValue(rootDir, out var cached))
+			return cached;
+		try
+		{
+			var xml = new XmlDocument();
+			xml.Load(rootDir + Path.DirectorySeparatorChar + "About" + Path.DirectorySeparatorChar + "Manifest.xml");
+			var tags = xml.SelectNodes("/Manifest/targetVersions/li").Cast<XmlNode>().Select(node => node.InnerText);
+			var result = tags
+				.Where(tag => VersionControl.IsWellFormattedVersionString(tag))
+				.Select(tag => VersionControl.VersionFromString(tag)).ToList();
+			cachedVersions[rootDir] = result;
+			return result;
+		}
+		catch (System.Exception)
+		{
+			cachedVersions[rootDir] = null;
+			return null;
+		}
+	}
+
+	static void VersionCompatible_Postfix(ModMetaData __instance, ref bool __result)
+	{
+		var taggedVersions = GetTaggedVersions(__instance.RootDir.FullName);
+		if (taggedVersions.NullOrEmpty()) return;
+		var currentVersion = VersionControl.CurrentVersion;
+		__result = taggedVersions
+			.Any(version =>
+				version.Major == currentVersion.Major &&
+				version.Minor == currentVersion.Minor &&
+				(version.Build == 0 || version.Build == currentVersion.Build));
+	}
+
+	static void SetWorkshopItemDataFrom_Postfix(UGCUpdateHandle_t updateHandle, WorkshopItemHook hook)
+	{
+		var taggedVersions = GetTaggedVersions(hook.Directory.FullName);
+		if (!taggedVersions.NullOrEmpty())
+		{
+			var tags = taggedVersions.Select(version => version.Major + "." + version.Minor);
+			tags.Add("Mod");
+			SteamUGC.SetItemTags(updateHandle, tags.Distinct().ToList());
+		}
 	}
 }
